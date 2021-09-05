6f02abd3b0886e15753676605d929dd91345fc24
WillyCheng <nightmarish205@yahoo.com.tw>
2021-09-05 13:16:34 +0800

    Add test

---
+++ test_project.py
@@ -0,0 +1,16 @@
+import unittest
+import function_plus as fp
+
+class TestPlusFunction(unittest.TestCase):
+
+    def test_positive(self):
+        self.assertEqual(fp.func_plus(1,2), 3)
+        self.assertEqual(fp.func_plus(3,99), 102)
+
+    def test_negative(self):
+        self.assertEqual(fp.func_plus(-1,-2), -3)
+        self.assertEqual(fp.func_plus(-3,-99), -102)
+
+if __name__ == "__main__":
+    unittest.main()
+

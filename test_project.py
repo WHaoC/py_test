2649edbd63ab37277afcaf128ec74ab763b27e0a
WillyCheng <nightmarish205@yahoo.com.tw>
2021-09-05 14:44:55 +0800

    Add test zero

--- test_project.py
+++ test_project.py
@@ -11,6 +11,10 @@ class TestPlusFunction(unittest.TestCase):
         self.assertEqual(fp.func_plus(-1,-2), -3)
         self.assertEqual(fp.func_plus(-3,-99), -102)
 
+    def test_zero(self):
+        self.assertEqual(fp.func_plus(0,0), 0)
+        self.assertEqual(fp.func_plus(3,0), 3)
+
 if __name__ == "__main__":
     unittest.main()
 
